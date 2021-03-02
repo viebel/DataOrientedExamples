33dfc113c65df9a8a67bb28e065e885f5b5ee3e7
Yehonathan Sharvit <viebel@gmail.com>
2021-03-02 07:26:46 +0200

    Update PMChapter3.java

--- src/main/java/org/organicdesign/dataOrient/chapter3/PMChapter3.java
+++ src/main/java/org/organicdesign/dataOrient/chapter3/PMChapter3.java
@@ -14,14 +14,14 @@ class PMChapter3 {
         return authorIds.map(authorId -> (String)authorByIds.get(authorId).get("name")).toImList();
     }
 
-    public static Map pBookInfo(ImMap<String,ImMap<String,ImMap<String,Object>>> catalogData, Map book) {
+    public static ImMap pBookInfo(ImMap catalogData, Map book) {
         return map(tup("title", book.get("title")),
                    tup("isbn", book.get("isbn")),
                    tup("authorNames", pAuthorNames(catalogData, book)));
     }
 
 
-    public static ImList<Tuple3<String,String,ImList<String>>> pSearchBooksByTitle(ImMap<String,ImMap<String,ImMap<String,Object>>> catalogData, String query) {
+    public static ImList<ImMap> pSearchBooksByTitle(ImMap<String,ImMap<String,ImMap<String,Object>>> catalogData, String query) {
         var allBooks = catalogData.get("booksByIsbn");
         var matchingBooks = allBooks.filter(kv -> ((String)kv.getValue()
                                                     .get("title"))
