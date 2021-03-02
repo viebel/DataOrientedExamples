54eac297cfdb9dde3e89f87352e8455cfa6b0bc9
Yehonathan Sharvit <viebel@gmail.com>
2021-03-02 08:14:26 +0200

    Update PMChapter3Test.java

--- src/test/java/org/organicdesign/dataOrient/chapter3/PMChapter3Test.java
+++ src/test/java/org/organicdesign/dataOrient/chapter3/PMChapter3Test.java
@@ -12,40 +12,27 @@ import static org.organicdesign.fp.StaticImports.*;
  */
 public class PMChapter3Test {
     @Test
-    public void testChapter3A() {
-        var watchmen = map(tup("isbn", "978-1779501127"),
-                           tup("title", "Watchmen"),
-                           tup("publicationYear", 1987),
-                           tup("authorIds", vec("alan-moore", "dave-gibbons")));
+    public void testChapter3A() {       
+        var catalog = map(tup("booksByIsbn", map(
+                                 tup("978-1779501127", map(
+                                     tup("isbn", "978-1779501127"),
+                                     tup("title", "Watchmen"),
+                                     tup("publicationYear", 1987),
+                                     tup("authorIds", vec("alan-moore", "dave-gibbons")))))),
+                          tup("authorsById", map(
+                              tup("alan-moore", map(
+                                  tup("name", "Alan Moore"),
+                                  tup("bookIsbns", vec("978-1779501127")))),
+                              tup("dave-gibbons", map(
+                                  tup("name", "Dave Gibbons"),
+                                  tup("bookIsbns", vec("978-1779501127")))))));
 
-        var alanMoore = map(tup("name", "Alan Moore"),
-                            tup("bookIsbns", vec("978-1779501127")));
-        var daveGibbons = map(tup("name", "Dave Gibbons"),
-                            tup("bookIsbns", vec("978-1779501127")));
-
-        var booksByIsbn = map(tup("978-1779501127", watchmen));
-
-        var authorsById = map(tup("alan-moore", alanMoore),
-                              tup("dave-gibbons", daveGibbons));
-
-        var catalog = map(tup("booksByIsbn", booksByIsbn),
-                          tup("authorsById", authorsById));
-
-        assertEquals("Watchmen",
-                     catalog.booksByIsbn().get("978-1779501127").title());
-
-        assertEquals(vec("Alan Moore", "Dave Gibbons"),
-                     pAuthorNames(catalog, watchmen));
-
-        assertEquals("Watchmen",
-                     pSearchBooksByTitle(catalog, "Watch").get(0)._1());
-
-        assertEquals("Watchmen",
-                     pSearchBooksByTitle2(catalog, "Watch").get(0).title());
-
-        assertEquals("Watchmen",
-                     pSearchBooksByTitle3(catalog, "Watch").get(0).title());
+        var expectedResult = vec(map(tup("isbn", "978-1779501127"),
+                                     tup("title", "Watchmen"),
+                                    tup("authorNames", vec("Alan Moore", "Dave Gibbons"))));
+        
+        assertEquals(expectedResult,
+                     pSearchBooksByTitle(catalog, "Watch"));
 
     }
-
 }
