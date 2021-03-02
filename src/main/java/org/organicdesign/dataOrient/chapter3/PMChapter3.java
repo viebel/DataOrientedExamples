29c08b84d3f172e116359d543c3654197df7a9ef
Yehonathan Sharvit <viebel@gmail.com>
2021-03-02 06:54:31 +0200

    Update PMChapter3.java

--- src/main/java/org/organicdesign/dataOrient/chapter3/PMChapter3.java
+++ src/main/java/org/organicdesign/dataOrient/chapter3/PMChapter3.java
@@ -1,12 +1,15 @@
 package org.organicdesign.dataOrient.chapter3;
 
 import org.organicdesign.fp.collections.ImList;
+import org.organicdesign.fp.collections.ImMap;
+
 
 import java.util.Map;
 
 class PMChapter3 {
 
-    public static ImList<String> pAuthorNames(ImMap<String,ImMap<String,ImMap<String,Object>>> catalogData, Map book) { 
+    public static ImList<String> pAuthorNames(ImMap<String,ImMap<String,ImMap<String,Object>>> catalogData,
+                                              ImMap book) { 
         var authorIds = (ImList<String>)book.get("authorIds");
         return authorIds.map(authorId -> (String)catalogData.get("authorsById")
                                                     .get(authorId)
@@ -23,26 +26,9 @@ class PMChapter3 {
 
     public static ImList<Tuple3<String,String,ImList<String>>> pSearchBooksByTitle(ImMap<String,ImMap<String,ImMap<String,Object>>> catalogData, String query) {
         var allBooks = catalogData.get("booksByIsbn");
-        var matchingBooks = allBooks.filter(kv -> kv.getValue()
-                                                    .title()
+        var matchingBooks = allBooks.filter(kv -> ((String)kv.getValue()
+                                                    .get("title"))
                                                     .contains(query));
         return matchingBooks.map(kv -> pBookInfo(catalogData, kv.getValue())).toImList();
-    }
-
-    public static ImList<PBookInfo> pSearchBooksByTitle2(PCatalog catalogData, String query) {
-        var allBooks = catalogData.booksByIsbn();
-        var matchingBooks = allBooks.filter(kv -> kv.getValue()
-                                                    .title()
-                                                    .contains(query));
-        return matchingBooks.map(kv -> pBookInfo2(catalogData, kv.getValue())).toImList();
-    }
-
-    public static ImList<PBookInfo> pSearchBooksByTitle3(PCatalog catalogData, String query) {
-        return catalogData.booksByIsbn()
-                          .map(Map.Entry::getValue)
-                          .filter(book -> book
-                                          .title()
-                                          .contains(query))
-                          .map(book -> pBookInfo2(catalogData, book)).toImList();
     }
 }
