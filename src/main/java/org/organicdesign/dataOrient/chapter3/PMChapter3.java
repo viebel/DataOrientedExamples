cf1f1d8c903fba55e1ed781120cf0e0a9ad39f93
Yehonathan Sharvit <viebel@gmail.com>
2021-03-03 10:16:50 +0200

    Use specific getter instead of static casting

--- src/main/java/org/organicdesign/dataOrient/chapter3/PMChapter3.java
+++ src/main/java/org/organicdesign/dataOrient/chapter3/PMChapter3.java
@@ -4,30 +4,63 @@ import org.organicdesign.fp.collections.ImList;
 import org.organicdesign.fp.collections.ImMap;
 import com.google.gson.Gson;
 
-class PMChapter3 {
-    public static ImList<String> pAuthorNames(ImMap catalogData,
-                                              ImMap book) {
-        var authorIds = (ImList<String>)book.get("authorIds");
-        var authorByIds = (ImMap<String,ImMap>)catalogData.get("authorsById");
-        return authorIds.map(authorId -> (String)authorByIds.get(authorId).get("name")).toImList();
+public class UniversalGet {
+    // TODO: move to methods in ImMap and ImList
+    // TODO: getInAs...
+    // TODO: getAsNumber, ...
+    public Integer getAsInteger(Map o, Object k) {
+        return (Integer)o.get((String)k);
     }
 
+    public String getAsString(Map o, Object k) {
+        return (String)o.get((String)k);
+    }
+
+    public Boolean getAsBoolean(Map o, Object k) {
+        return (Boolean)o.get((String)k);
+    }
+
+    public ImMap getAsMap(Map o, Object k) {
+        return (ImMap)o.get((String)k);
+    }
+
+    public ImMap<String, ImMap> getAsMapOfMaps(ImMap o, Object k) {
+        return (ImMap<String,ImMap>)o.get((String)k);
+    }
+
+    public ImList getAsList(Map o, Object k) {
+        return (ImList)o.get((String)k);
+    }
+}
+
+class PMChapter3 {
+    @SuppressWarnings("unchecked")
+    public static ImList<String> pAuthorNames(ImMap catalogData,
+                                              ImMap book) {
+        var authorIds = getAsList(book, "authorIds");
+        var authorByIds = getAsMap(catalogData, "authorsById");
+        return authorIds.map(authorId -> getAsMap(authorByIds, authorId).get("name")).toImList();
+    }
+
+    @SuppressWarnings("unchecked")
     public static ImMap pBookInfo(ImMap catalogData, ImMap book) {
         return map(tup("title", book.get("title")),
                    tup("isbn", book.get("isbn")),
                    tup("authorNames", pAuthorNames(catalogData, book)));
     }
 
+    @SuppressWarnings("unchecked")
     public static ImList<ImMap> pSearchBooksByTitle(ImMap catalogData, String query) {
-        return ((ImMap<String,ImMap>)catalogData.get("booksByIsbn"))
+        return getAsMapOfMaps(catalogData, "booksByIsbn")
             .values()
-            .filter(book -> ((String)book.get("title")).contains(query))
+            .filter(book -> getAsString(book, "title").contains(query))
             .map(book -> pBookInfo(catalogData, book))
             .toImList();
     }
 
+    @SuppressWarnings("unchecked")
     public static String pSearchBooksByTitleJSON(ImMap libraryData, String query) {
-        var books = pSearchBooksByTitle((ImMap)libraryData.get("catalog"), query);
+        var books = pSearchBooksByTitle(getAsMap(libraryData, "catalog"), query);
         return new Gson().toJson(books);
     }
 }
